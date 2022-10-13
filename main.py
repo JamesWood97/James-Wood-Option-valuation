2a6b5cee332c4f59efa057d1115425b7379d84c7
JamesWood97 <james.john.wood97@gmail.com>
2022-10-13 15:50:59 +0100

    added valuing options with bimomail tress, including asking for the relevant parameters from the user e.g volatilty, strike price etc

--- main.py
+++ main.py
@@ -19,18 +19,36 @@ def get_choice(question:str, choice_strings:list[str]):
             return choice_number-1
 
 
+
+
+
 def main():
-    avalible_methods_for_option_type = {"european":["Binomial", "Trinomial", "Finite difference", "Monte-Carlo"],
-                                        "american":["binomial", "finite difference"]}
+    avalible_methods_for_option_type = {"european": ["binomial", "trinomial", "finite difference", "monte carlo"],
+                                        "american": ["binomial", "finite difference"]}
+
+    option_type_index = get_choice("Option type:",["European", "American"])
+    is_american = bool(option_type_index)
+    method_to_use = get_choice("Method to use:", avalible_methods_for_option_type[{True: "american", False: "european"}])
 
     r = get_value("Interest rate: ")
     sigma = get_value("Volatility: ")
-    E = get_value("Strike Price: ")
     S = get_value("Spot Price: ")
+    T = get_value("Years until expiry: ")
+    q = get_value("Dividend rate")
 
-    option_type_index = get_choice("Option type:",["European", "American"])
-    option_type = {0: "european", 1: "american"}[option_type_index]
-    method_to_use = get_choice("Method to use:", avalible_methods_for_option_type[option_type])
+    if method_to_use == "binomial":
+        E = get_value("Strike Price: ")
+        number_of_steps = get_value("Number of Steps: ", conversion_function=lambda x: float(x))
+        estimated_value = k_nomial(T, sigma, r, q, S, E, lambda x:max(x-E,0), number_of_steps=number_of_steps, k=2,
+                                   american=is_american, lower_bound=None, upper_bound=None)
+
+
+
+    print(estimated_value)
+
+
+
+
 
 if __name__ == "__main__":
     main()
