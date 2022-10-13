19c46e5fc63411ee4b9284a55a0ca30b6942d195
JamesWood97 <james.john.wood97@gmail.com>
2022-10-13 17:42:00 +0100

    added fintie difference method as a choice

--- main.py
+++ main.py
@@ -33,30 +33,43 @@ def main():
     option_type_index = get_choice("Option type:",["European", "American"])
     is_american = bool(option_type_index)
     method_to_use = get_choice("Method to use:",
-                               avalible_methods_for_option_type[{True: "american", False: "european"}[is_american]])
+                               avalible_methods_for_option_type[{True: "american", False: "european"}[is_american]],
+                               return_string=True)
 
     r = get_value("Interest rate: ")
     sigma = get_value("Volatility: ")
     S = get_value("Spot Price: ")
     T = get_value("Years until expiry: ")
-    q = get_value("Dividend rate")
+    #q = get_value("Dividend rate")
+    lower_barrier = None
+    upper_barrier = None
+    payoff = lambda x:max(x-E,0)
 
-    if method_to_use == 0:
+    if method_to_use == "binomial":
         E = get_value("Strike Price: ")
         number_of_steps = get_value("Number of Steps: ", conversion_function=lambda x: int(x))
         mode = get_choice("Binomial method to use:", ["Cox-Ross-Rubenstein", "Jarrow-Rudd", "Leisen-Reimer"],
                           return_string=True)
-        estimated_value = k_nomial(T, sigma, r, q, S, E, mode, lambda x:max(x-E,0), number_of_steps=number_of_steps,
-                                   k=2, american=is_american, lower_bound=None, upper_bound=None)
+        estimated_value = k_nomial(T, sigma, r, q, S, E, mode, payoff, number_of_steps=number_of_steps,
+                                   k=2, american=is_american, lower_bound=lower_barrier, upper_bound=upper_barrier)
 
 
-    elif method_to_use == 1:
+    elif method_to_use == "trinomial":
         E = get_value("Strike Price: ")
         number_of_steps = get_value("Number of Steps: ", conversion_function=lambda x: int(x))
         mode = get_choice("Binomial method to use:", ["Cox-Ross-Rubenstein", "Jarrow-Rudd", "Leisen-Reimer"],
                           return_string=True)
-        estimated_value = k_nomial(T, sigma, r, q, S, E, mode, lambda x:max(x-E,0), number_of_steps=number_of_steps,
-                                   k=3, american=is_american, lower_bound=None, upper_bound=None)
+        estimated_value = k_nomial(T, sigma, r, q, S, E, mode, payoff, number_of_steps=number_of_steps,
+                                   k=3, american=is_american, lower_bound=lower_barrier, upper_bound=upper_barrier)
+
+
+    elif method_to_use == "finite differences":
+        E = get_value("Strike Price: ")
+        number_of_steps = get_value("Number of Steps: ", conversion_function=lambda x: int(x))
+        mode = get_choice("Fintie difference method to use:", ["Explicit", "Implicit", "Crank-Nicolson"],
+                          return_string=True)
+        fdm_obj = FDM(T, sigma, r, q, S, E, payoff, number_of_steps, number_of_t_nodes=None, dx=None, mode =mode, lower_barrier=lower_barrier, upper_barrier=upper_barrier, american=is_american)
+        estimated_value = fdm_obj.get_value_estimation_at_start(S)
 
     print(estimated_value)
 
