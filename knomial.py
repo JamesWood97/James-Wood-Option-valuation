c440b8292367b1e584096e662d08b6794f175bd4
JamesWood97 <james.john.wood97@gmail.com>
2022-10-15 00:34:48 +0100

    Added option class to tree methods in place of taking in all parameters as arguments

--- knomial.py
+++ knomial.py
@@ -1,9 +1,11 @@
 from finance import *
 
 
-def k_nomial(T, sigma, r, q, S, E, mode, payoff, number_of_steps=1, k=2, american=False, lower_bound=None,upper_bound=None):
-    s = sigma
-
+def k_nomial(option: Option, mode, number_of_steps=1, k=2):
+    T = option.time_until_expiry
+    E = option.strike_price
+    sigma = option.volatility
+    r = option.interest_rate
     dt = T / number_of_steps
     binomial_number_of_steps = number_of_steps*(k-1)
     u,d,pu = get_binomial_values(T,binomial_number_of_steps,sigma,r,q,E,S,mode)
@@ -13,14 +15,14 @@ def k_nomial(T, sigma, r, q, S, E, mode, payoff, number_of_steps=1, k=2, america
 
 
     #create the underlying tree
-    S_values = [[S]]
+    S_values = [[option.spot_price]]
     for step_number in range(number_of_steps):
         previous_S_values = S_values[-1]
         new_S_values = [i*previous_S_values[0] for i in u_values]
         for i in range(1,len(previous_S_values)):
             new_S_values.append(previous_S_values[i] * u_values[-1])
         S_values.append(new_S_values)
-    option_values = [payoff(x) for x in S_values[-1]]
+    option_values = [option.payoff(x) for x in S_values[-1]]
 
 
     #backtrack
@@ -34,16 +36,18 @@ def k_nomial(T, sigma, r, q, S, E, mode, payoff, number_of_steps=1, k=2, america
             for i in range(k):
                 val += p_values[i]*previous_option_values[value_index+i]
             val *= exp(-1*r*dt)
-            if upper_bound is not None:
-                if underlying_S >= upper_bound:
+
+            if option.upper_barrier is not None:
+                if underlying_S >= option.upper_barrier:
                     option_values.append(0)
                     continue
-            if lower_bound is not None:
-                if underlying_S <= lower_bound:
+            if option.lower_barrier is not None:
+                if underlying_S <= option.lower_barrier:
                     option_values.append(0)
                     continue
-            if american:
-                option_values.append(max(payoff(underlying_S,E),val))
+
+            if option.is_american:
+                option_values.append(max(option.payoff(underlying_S), val))
             else:
                 option_values.append(val)
     return option_values[0]
